b7b7094afd2853bd10c0280e56f78353fda78e1a
Cory Burgett <cmburget@gmail.com>
2015-08-29 23:53:11 -0400

    Add impl Display for Token

--- oxidation/src/parser/tokens.rs
+++ oxidation/src/parser/tokens.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter, Error};
 
 #[derive(Clone,PartialEq,Eq,Debug)]
 pub enum Token {
@@ -57,3 +58,67 @@ pub enum Token {
   EOF
 }
 
+impl Display for Token {
+  fn fmt(&self, f : &mut Formatter) -> Result<(), Error> {
+    let repr = match *self {
+      Token::And => "'and'",
+      Token::Break => "'break'",
+      Token::Do => "'do'",
+      Token::Else => "'else'",
+      Token::Elseif => "'elseif'",
+      Token::End => "'end'",
+      Token::False => "'false'",
+      Token::For => "'for'",
+      Token::Function => "'function'",
+      Token::If => "'if'",
+      Token::In => "'in'",
+      Token::Local => "'local'",
+      Token::Nil => "'nil'",
+      Token::Not => "'not'",
+      Token::Or => "'or'",
+      Token::Repeat => "'repeat'",
+      Token::Return => "'return'",
+      Token::Then => "'then'",
+      Token::True => "'true'",
+      Token::Until => "'until'",
+      Token::While => "'while'",
+
+      Token::Plus => "'+'",
+      Token::Minus => "'-'",
+      Token::Times => "'*'",
+      Token::Divide => "'/'",
+      Token::Mod => "'%'",
+      Token::Exp => "'^'",
+      Token::Len => "'#'",
+      Token::Eq => "'=='",
+      Token::Neq => "'~='",
+      Token::Leq => "'<='",
+      Token::Geq => "'>='",
+      Token::Lt => "'<'",
+      Token::Gt => "'>'",
+      Token::Assign => "'='",
+      Token::LParen => "'('",
+      Token::RParen => "')'",
+      Token::LBrace => "'{'",
+      Token::RBrace => "'}'",
+      Token::LSquare => "'['",
+      Token::RSquare => "']'",
+      Token::Semi => "';'",
+      Token::Colon => "':'",
+      Token::Comma => "','",
+      Token::Dot => "'.'",
+      Token::Dotdot => "'..'",
+      Token::Dotdotdot => "'...'",
+
+      Token::String(_) => "string literal",
+      Token::Number(_) => "number literal",
+      Token::Name(_)   => "name",
+
+      Token::EOF => "<eof>"
+    };
+
+    try!(f.write_str(repr));
+    
+    Ok(())
+  }
+}
